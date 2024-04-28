9f3c3f5bb3b8bda63918c8a8c38a1684234207ae
Artur Gorevyi <gorartur997@gmail.com>
2024-04-28 18:43:28 +0300

    DB connection

--- server/index.js
+++ server/index.js
@@ -1,4 +1,13 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const app = express()
 
-alert('test');
-alert('test');
-alert('test');
+// connect DB
+mongoose
+    .connect('mongodb+srv://admin:admin@cluster0.znghj9j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+    .then(() => console.log('DB is connected'))
+    .catch((err) => console.log('DB error', err))
+
+// connect app
+app.listen(process.env.PORT, () => console.log('Server is connected'))
