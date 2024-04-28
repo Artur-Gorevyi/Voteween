b2316c8c9ac02627c5d3c953f0fb2b8046e429d3
Artur Gorevyi <gorartur997@gmail.com>
2024-04-28 17:47:11 +0300

    Test

--- server/index.js
+++ server/index.js
@@ -1,10 +1,1 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv').config()
-const cors = require('cors')
-const app = express()
-
-// connect DB
-
-
-// connect 
+alert('test');
