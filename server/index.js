c2109a9cb4c1805bc008cd1dd7f773ef73af79b6
Artur Gorevyi <gorartur997@gmail.com>
2024-05-09 14:37:30 +0300

    UI for Upload and Users

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const commentController = require('./controllers/commentController')
+const uploadController = require('./controllers/uploadController')
 const authController = require('./controllers/authController')
 const userController = require('./controllers/userController')
 const postController = require('./controllers/postController')
-const commentController = require('./controllers/commentController')
 const dotenv = require('dotenv').config()
-const app = express()
+const mongoose = require('mongoose')
+const express = require('express')
 const cors = require('cors')
+const app = express()
 
 // connect DB
 mongoose.set('strictQuery', false);
@@ -15,6 +16,7 @@ mongoose
     .then(() => console.log('DB is connected'))
     .catch((err) => console.log('DB error', err))
 
+app.use('/images', express.static('public/images'))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -22,6 +24,7 @@ app.use('/auth', authController)
 app.use('/user', userController)
 app.use('/post', postController)
 app.use('/comment', commentController)
+app.use('/upload', uploadController)
 
 // connect app
 app.listen(process.env.PORT, () => console.log('Server is connected'))
